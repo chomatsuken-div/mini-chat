553e0faccd6af2ab46d583c8d0e81285e0e92515
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-11-20 23:18:01 +0900

    default order change group message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   has_many :messages
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
