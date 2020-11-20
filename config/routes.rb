ee8d3d79c331c4cddc81fa67f02c950f4e77f472
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-11-20 14:36:20 +0900

    set routing messages#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :groups, only: [:index, :create, :update, :destroy]
+      resources :groups, only: [:index, :create, :update, :destroy] do
+        resources :messages, only: [:create]
+      end
     end
   end
 end
