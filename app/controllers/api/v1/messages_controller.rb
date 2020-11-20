438be11b5d265072063837aa08b050a8f999a6e7
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-11-20 14:37:31 +0900

    request controller api/v1/messages#create

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MessagesController < ApplicationController
+  def create
+    group = Group.find(params[:group_id])
+    message = Message.new(message_params)
+    if message.save
+      render json: message, status: 200
+    else
+      render json: { errors: message.errors.full_messages }
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
