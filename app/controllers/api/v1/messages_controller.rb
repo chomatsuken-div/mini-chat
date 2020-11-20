37fff3e60d22e0c01082e04424d5d1ab5fd3659e
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-11-20 14:56:01 +0900

    rspec request api/v1/messages#create

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MessagesController < ApplicationController
   def create
     group = Group.find(params[:group_id])
-    message = Message.new(message_params)
+    message = group.messages.new(message_params)
     if message.save
       render json: message, status: 200
     else
--- spec/requests/api/v1/messages_request_spec.rb
+++ spec/requests/api/v1/messages_request_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Messages", type: :request do
+  let(:group) { create(:group) }
 
+  before do
+    @api_v1_group_messages_path_json = api_v1_group_messages_path(group) + '.json' # api/vi/messages#create用のURLを生成
+  end
+
+  describe 'メッセージ作成機能(api/v1/messages#create)' do
+    let(:message) { create(:message) }
+
+    subject { post @api_v1_group_messages_path_json, params: { message: @message_params } } # api/v1/messages#createへリクエストを送る
+
+    context '新規作成に成功する場合' do
+      before do
+        @message_params = {content: message.content}
+      end
+
+      it 'ステータス200を返す' do
+        subject
+        expect(response.status).to eq(200)
+      end
+
+      it 'メッセージの数が+1される' do
+        expect {subject}.to change(Message, :count).by(1)
+      end
+    end
+
+    context '新規作成に失敗する場合' do
+      context 'メッセージが空で入力された場合' do
+        before do
+          @message_params = {content: ''}
+          subject
+          @json = JSON.parse(response.body)
+        end
+
+        it 'ステータス200を返す' do
+          expect(response.status).to eq(200)
+        end
+
+        it 'メッセージの数が±0を返す' do
+          expect {subject}.to change(Message, :count).by(0)
+        end
+
+        it 'エラーメッセージを返す' do
+          expect(@json["errors"]).to include("メッセージ内容を入力してください")
+        end
+      end
+    end
+  end
 end
